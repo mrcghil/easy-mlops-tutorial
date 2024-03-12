3447e38493539c7da486de6c0be648ff30bdefd2
mghilar <marco.ghilardi@ralphlauren.com>
2024-03-12 17:36:34 +0000

    Saved recipe: EASYMLOPSDEMO.compute_collected_data

--- recipes/compute_collected_data.py
+++ recipes/compute_collected_data.py
@@ -3,12 +3,34 @@ import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
-# Read recipe inputs
-sample_batches = dataiku.Folder("zY3ZURve")
-sample_batches_info = sample_batches.get_info()
+# Read recipe inputs from reference/test/historical dataset
+sample_data = dataiku.Dataset("sample_data")
+sample_data_df = sample_data.get_dataframe()
+
+# Drop the columns that cannot be used for modelling
+sample_data_df = sample_data_df.drop(columns=['long_trend', 'short_trend', 'noise', 'price_sensitivity', 'score'])
+# Add a tag to the data
+sample_data_df['source'] = "reference"
+
+# Read inputs from the live dateset (results of model being active)
+try:
+	results_from_model = dataiku.Dataset("results_from_model")
+	results_from_model_df = results_from_model.get_dataframe()
+	# Drop the columns that cannot be used for modelling
+	results_from_model_df = results_from_model_df.drop(columns=['long_trend', 'short_trend', 'noise', 'price_sensitivity', 'score'])
+	# Add a tag to the data
+	results_from_model_df['source'] = "live"
+except Exception as exc:
+	print(exc.args)
+	print("Data from live model not available.")
+	live_data = False
 
 
-collected_data_df = pd.DataFrame()
+# Collate the two sets
+if live_data:
+	collected_data_df = pd.concat([sample_data_df, results_from_model_df])
+else:
+	collected_data_df = sample_data_df
 
 
 # Write recipe outputs

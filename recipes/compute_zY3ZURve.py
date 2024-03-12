3723630387441a0346b93194b3e9528d427142e1
mghilar <marco.ghilardi@ralphlauren.com>
2024-03-12 12:33:50 +0000

    Saved recipe: EASYMLOPSDEMO.compute_zY3ZURve

--- recipes/compute_zY3ZURve.py
+++ recipes/compute_zY3ZURve.py
@@ -10,3 +10,13 @@ sample_batches_info = sample_batches.get_info()
 
 # Path in folder
 data_folder = dataiku.get_custom_variables()["batches_path"]
+
+# Model
+def get_features(current_time: datetime.datetime) -> pd.DataFrame:
+	pass
+
+def get_measurement(features: pd.DataFrame) -> pd.Series:
+	pass
+
+# Produce the data and save to the folder
+

2ca13331e65999cea9fcf766142c5ee5751f12eb
mghilar <marco.ghilardi@ralphlauren.com>
2024-03-12 14:48:31 +0000

    Recipe EASYMLOPSDEMO.compute_zY3ZURve saved through notebook notebook editor for compute_zY3ZURve

--- recipes/compute_zY3ZURve.py
+++ recipes/compute_zY3ZURve.py
@@ -1,11 +1,13 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import datetime
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from dataiku import pandasutils as pdu
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Connect to folder
 sample_batches = dataiku.Folder("zY3ZURve")
 sample_batches_info = sample_batches.get_info()
@@ -13,12 +15,57 @@ sample_batches_info = sample_batches.get_info()
 # Path in folder
 data_folder = dataiku.get_custom_variables()["batches_path"]
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Model
-def get_features(current_time: datetime.datetime) -> pd.DataFrame:
-	pass
+seed = 112233
+np.random.seed(seed)
+reference_time = datetime.datetime(2024, 1, 1)
 
-def get_measurement(features: pd.DataFrame) -> pd.Series:
-	pass
+def get_features(current_time: datetime.datetime, fixed_delta = "random", size:tuple = (1000,)) -> pd.DataFrame:
+    # Time
+    time_delta = current_time - reference_time
+    timestamps = pd.date_range(current_time, periods=size[0], freq="s")
+    
+    # Trend increses in time to saturation
+    long_trend = min((0.008 * time_delta.total_seconds() / (24 * 60 * 60)), 1) * np.ones(size)
+    
+    # Intraday
+    short_trend = 0.1 * np.sin( 2 * np.pi * (time_delta.seconds / 3600) / 24 ) * np.ones(size)
+    
+    # Price delta
+    if type(fixed_delta) == str and fixed_delta == "random":
+        price_delta = np.random.uniform(-0.5, 0.5, size)
+    elif type(fixed_delta) == int or type(fixed_delta) == float:
+        price_delta = fixed_delta * np.ones(size)
+    
+    # Random noise
+    noise_a = np.random.normal(0, 0.02, size)
+    
+    df = pd.DataFrame(
+        {
+            "timestamps": timestamps,
+            "long_trend": long_trend.tolist(),
+            "short_trend": short_trend.tolist(),
+            "noise": noise_a.tolist(),
+            "price_delta": price_delta.tolist(),
+        }
+    )
+    
+    return df
 
+def get_outcome(features: pd.DataFrame) -> pd.Series:
+    # Price dependency
+    price_sensitivity = - 0.2 / (1 + np.exp(-features['price_delta']))
+    outcome = features['long_trend'] + features['short_trend'] + features['price_delta'] + features['noise']
+    return outcome
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Produce the data and save to the folder
-current_time = datetime.datetime.now().timestamp()
+current_time = datetime.datetime.now()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Produce features
+features = get_features(current_time, "random", (1000,))
+print(features.head(20))
+features['purchased'] = get_outcome(features)
+print(features.describe())

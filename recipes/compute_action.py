af9a64aa5846e3b03f3684a2026ce9aedd38d799
mghilar <marco.ghilardi@ralphlauren.com>
2024-03-12 17:43:03 +0000

    Saved recipe: EASYMLOPSDEMO.compute_action

--- recipes/compute_action.py
+++ recipes/compute_action.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import dataiku
-import pandas as pd, numpy as np
+import pandas as pd
+import datetime
+import numpy as np
 from dataiku import pandasutils as pdu
 
 # Read recipe inputs
@@ -8,7 +10,10 @@ model_versions = dataiku.Folder("cjQ040KC")
 model_versions_info = model_versions.get_info()
 
 
-action_df = pd.DataFrame()
+action_df = pd.DataFrame({
+	"timestamp": [pd.Timestamp(year=2024, month=3, day=12)],
+	"optimal_price_delta": [-0.9]
+})
 
 
 # Write recipe outputs

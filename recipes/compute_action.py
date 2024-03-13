6d1e52ff81453efa4a9b3f303ae79ae8ed87e456
mghilar <marco.ghilardi@ralphlauren.com>
2024-03-13 16:50:19 +0000

    Recipe EASYMLOPSDEMO.compute_action saved through notebook notebook editor for compute_action

--- recipes/compute_action.py
+++ recipes/compute_action.py
@@ -1,35 +1,61 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd
 import datetime
+import json
 import numpy as np
 from dataiku import pandasutils as pdu
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 model_versions = dataiku.Folder("cjQ040KC")
 model_versions_info = model_versions.get_info()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Get the latest model build path
-with lts_storage.get_download_stream(full_payment_list_file) as f:
-	full_payment_list = json.loads(
-		f.read().decode()
-	)
+with model_versions.get_download_stream('latest_model.json') as f:
+    latest_model_info = json.loads(
+        f.read().decode()
+    )
+print(latest_model_info)
 
-# Load the model score-cards
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Find the model score-cards
+model_versions_list = model_versions.list_paths_in_partition()
 
+ALL = False
 
-# Decide which model is best to run
+if ALL:
+    model_card_paths = [path for path in model_versions_list if "latest_model.json" not in path ]
+else:
+    model_card_paths = [path for path in model_versions_list if latest_model_info['path'] in path ]
 
+# Load the model scorecards
+model_cards = []
+for model_card_path in model_card_paths:
+    with model_versions.get_download_stream(model_card_path) as f:
+        model_card = json.loads(
+            f.read().decode()
+        )
+    model_cards.append(model_card)
 
-# Predict the model output
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Model cards flattening (could be done better to and recursively once the data are fuller)
+actions = [card['outputs']['action'] for card in model_cards]
+scores = [card['scores']['score'] for card in model_cards]
+timestamps = [card['timestamp'] for card in model_cards]
 
-
-# Pass the model output and other relevant info to the outputs
-action_df = pd.DataFrame({
-	"timestamp": [pd.Timestamp(year=2024, month=3, day=12)],
-	"optimal_price_delta": [-0.9]
+models_collection = pd.DataFrame({
+    "timestamp": timestamps,
+    "optimal_price_delta": actions,
+    "scores": scores,
 })
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Decide which model is best to run
+models_collection = models_collection.sort_values('scores', ascending=False)
 
 # Write recipe outputs
 action = dataiku.Dataset("action")
-action.write_with_schema(action_df)
+action.write_with_schema(models_collection)

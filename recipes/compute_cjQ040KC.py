a578a43190a0896f140f5773597e689a5a752490
mghilar <marco.ghilardi@ralphlauren.com>
2024-03-13 16:08:32 +0000

    Saved recipe: EASYMLOPSDEMO.compute_cjQ040KC

--- recipes/compute_cjQ040KC.py
+++ recipes/compute_cjQ040KC.py
@@ -4,6 +4,7 @@ import dataiku
 import pandas as pd
 import numpy as np
 import datetime
+import json
 from sklearn.linear_model import LogisticRegression
 from matplotlib import pyplot as plt
 from dataiku import pandasutils as pdu
